b4958a5316f25a465248d23aa0aee076fd6c479d
Atul Kamath <realatulkamath@gmail.com>
2022-02-11 18:37:51 +0400

    Time: 141 ms (84.69%), Space: 46.8 MB (33.92%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    let result = 0;
+    let roman = {"I":1,"V":5,"X":10,"L":50,"C":100,"D":500,"M":1000}
+    for (let i = 0; i <s.length; i++) {
+        if (i + 1 < s.length && roman[s[i]] < roman[s[i + 1]]) {
+            result -= roman[s[i]]
+        }
+        else {
+            result += roman[s[i]]
+        }
+    }
+    return result;
+};
